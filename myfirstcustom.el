d841d2e59162f08c5bc70fd9f93bd207b0c39b33
Craig Lovell <cxgslegend@gmail.com>
2016-12-28 22:09:33 -0500

    Initial commit

---
+++ myfirstcustom.el
@@ -0,0 +1,60 @@
+;;; Begin initialization
+;; Turn off mouse interface early in startup to avoid momentary display
+(when window-system
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (tooltip-mode -1))
+
+(setq inhibit-startup-message t)
+(setq initial-scratch-message "")
+
+(require 'package)
+(setq package-archives '(("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
+						 ("marmalade" . "http://marmalade-repo.org/packages/")
+						 ("org" . "http://orgmode.org/elpa/")
+						 ("gnu" . "http://elpa.gnu.org/packages/")))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+(defun ensure-package-installed (&rest packages)
+  "Assure every package is installed, ask for installation if it’s not.
+
+  Return a list of installed packages or nil for every skipped package."
+  (mapcar
+   (lambda (package)
+     (if (package-installed-p package)
+         nil
+       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+           (package-install package)
+         package)))
+   packages))
+
+;; Make sure to have downloaded archive description.
+(or (file-exists-p package-user-dir)
+    (package-refresh-contents))
+
+;; Assuming you wish to install "iedit" and "magit"
+(ensure-package-installed 'iedit
+			  'magit
+			  'evil
+			  'projectile)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (molokai)))
+ '(custom-safe-themes
+   (quote
+    ("b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'evil)
+(evil-mode t)
